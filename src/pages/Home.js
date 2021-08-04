c7d40534dc9a32756f9c538c4821033da8895fa0
Ashar Farooq <ashar744@gmail.com>
2021-08-04 05:02:26 -0400

    added basic gql query

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,38 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const query = gql`
+{
+    countries {
+  name
+  capital
+  emoji
+}
+}
+`;
+
 
 function Home() {
+
+const { data, loading, error } = useQuery(query);
+
   return (
-    <div className="App">
-         <h1>Home</h1>
+    <div className="Home">
+         <h1>Countries</h1>
+
+        <div className="countries">
+          {loading && <h3>Loading...</h3>}
+          {error && <h3>Error: {error.message} </h3>}
+          {data && data.countries.map((each_country, key) => {
+              return (
+              <div key={key}> 
+              <h2>{each_country.name} {each_country.emoji}</h2>
+               <h4>{each_country.capital}  </h4>
+
+              </div>)
+          })}
+        </div>
+
     </div>
   );
 }
