879f9d2bc666e18d686eb57246c764a5f0664e61
Ashar Farooq <ashar744@gmail.com>
2021-08-04 04:38:40 -0400

    added basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router } from "react-router-dom";
+import  { BrowserRouter , Switch, Route} from "react-router-dom";
+import Home from "./pages/Home.js";
+import Query from "./pages/Query.js";
+
 
 function App() {
   return (
     <div className="App">
-     
+     <BrowserRouter>
+
+     <Switch>
+
+        <Route path="/" exact component={Home} /> 
+        <Route path="/query" exact component={Query} /> 
+
+     </Switch>
+
+     </BrowserRouter>
     </div>
   );
 }
